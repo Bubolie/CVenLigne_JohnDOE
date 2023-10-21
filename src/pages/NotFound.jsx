3e819568c9b3e5d46bc354eb33c66340bee44555
Bubolie <elodie41.m@gmail.com>
2023-10-21 10:32:54 +0300

    Création d'une page 404

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <p className="m-5 text-primary">
+    La page que vous recherchez n'exite pas, merci de revenir à la page
+    d'acceuil du site.
+  </p>
+);
+
+export default NotFound;
